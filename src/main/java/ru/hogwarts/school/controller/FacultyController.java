45298fc8c7f11ff028c63468d825dffee3ebe4f5
anna-ermakova <korovanna8@mail.ru>
2023-06-26 17:18:08 +0300

    FacultyController готов

--- src/main/java/ru/hogwarts/school/controller/FacultyController.java
+++ src/main/java/ru/hogwarts/school/controller/FacultyController.java
@@ -8,43 +8,24 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import ru.hogwarts.school.dto.FacultyDtoIn;
 import ru.hogwarts.school.dto.FacultyDtoOut;
 import ru.hogwarts.school.entity.Faculty;
 import ru.hogwarts.school.service.FacultyService;
 
 import javax.validation.Valid;
-import java.util.Collection;
+import java.util.List;
 
 @RestController
 @RequestMapping("/faculties")
-@RequiredArgsConstructor
 @Tag(name = "Факультеты.", description = "CRUD-операции и другие эндпоинты для работы с факультетами.")
 public class FacultyController {
 
     private final FacultyService facultyService;
 
-    @GetMapping("/{id}")
-    @Operation(summary = "Получение факультета по id.")
-    @ApiResponses(value = {
-            @ApiResponse(
-                    responseCode = "200",
-                    description = "Факультет получен.",
-                    content = {
-                            @Content(
-                                    mediaType = "application/json",
-                                    schema = @Schema(implementation = Faculty.class)
-                            )
-                    }
-            )
-    })
-    @Parameters(value = {@Parameter(name = "id", example = "1")})
-    ResponseEntity<FacultyDtoOut> getFaculty(@PathVariable("id") Long id) {
-        Faculty faculty = facultyService.getFacultyById(id);
-        return ResponseEntity.ok(faculty);
+    public FacultyController(FacultyService facultyService) {
+        this.facultyService = facultyService;
     }
 
     @PostMapping
@@ -61,8 +42,8 @@ public class FacultyController {
                     }
             )
     })
-    ResponseEntity<FacultyDtoOut> addFaculty(@Valid @RequestBody FacultyDtoIn facultyDtoIn) {
-        return ResponseEntity.ok(facultyService.create(facultyDtoIn));
+    public FacultyDtoOut create(@Valid @RequestBody FacultyDtoIn facultyDtoIn) {
+        return facultyService.create(facultyDtoIn);
     }
 
     @PutMapping("/{id}")
@@ -80,12 +61,27 @@ public class FacultyController {
             )
     })
     @Parameters(value = {@Parameter(name = "id", example = "1")})
-    public ResponseEntity<FacultyDtoOut> updateFaculty(@PathVariable("id") Long id, @Valid @RequestBody FacultyDtoIn facultyDtoIn) {
-        Faculty facultyReturn = facultyService.update(id, facultyDtoIn);
-        if (facultyReturn == null) {
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
-        }
-        return ResponseEntity.ok(facultyReturn);
+    public FacultyDtoOut update(@PathVariable("id") Long id, @Valid @RequestBody FacultyDtoIn facultyDtoIn) {
+        return facultyService.update(id, facultyDtoIn);
+    }
+
+    @GetMapping("/{id}")
+    @Operation(summary = "Получение факультета по id.")
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "Факультет получен.",
+                    content = {
+                            @Content(
+                                    mediaType = "application/json",
+                                    schema = @Schema(implementation = Faculty.class)
+                            )
+                    }
+            )
+    })
+    @Parameters(value = {@Parameter(name = "id", example = "1")})
+    public FacultyDtoOut getFaculty(@PathVariable("id") Long id) {
+        return facultyService.getFacultyById(id);
     }
 
 
@@ -97,30 +93,12 @@ public class FacultyController {
                     description = "Факультет удален."
             )})
     @Parameters(value = {@Parameter(name = "id", example = "1")})
-    ResponseEntity<FacultyDtoOut> removeFaculty(@PathVariable("id") Long id) {
-        return ResponseEntity.ok(facultyService.delete(id));
+    public FacultyDtoOut delete(@PathVariable("id") Long id) {
+        return facultyService.delete(id);
     }
 
 
-    @GetMapping("/{all}")
-    @Operation(summary = "Получение всех факультетов.")
-    @ApiResponses(value = {
-            @ApiResponse(
-                    responseCode = "200",
-                    description = "Все факультеты получены.",
-                    content = {
-                            @Content(
-                                    mediaType = "application/json",
-                                    schema = @Schema(implementation = Faculty.class)
-                            )
-                    }
-            )
-    })
-    public Collection<FacultyDtoOut> getAllFaculties(@PathVariable("all") String all) {
-        return this.facultyService.getAllFaculties();
-    }
-
-    @GetMapping("/color/{color}")
+    @GetMapping
     @Operation(summary = "Получение факультетов по цвету.")
     @ApiResponses(value = {
             @ApiResponse(
@@ -134,12 +112,7 @@ public class FacultyController {
                     }
             )
     })
-    public ResponseEntity<Collection<FacultyDtoOut>> getAllFacultiesByColor(@RequestParam(required = false) String color) {
-        Collection<Faculty> result = facultyService.getAllFacultyByColor(color);
-        if (result.size() == 0) {
-            return ResponseEntity.notFound().build();
-        }
-        return ResponseEntity.ok(result);
+    public List<FacultyDtoOut> getAllFacultiesByColor(@RequestParam(required = false) String color) {
+        return facultyService.getAllFacultyByColor(color);
     }
-
 }
